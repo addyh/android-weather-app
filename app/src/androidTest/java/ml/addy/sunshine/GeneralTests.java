e16d168db18ec811c376e97f394f4d0dec59b439
addyh <addisonhatcher@gmail.com>
2018-11-01 13:24:54 -0400

    general tests

---
+++ app/src/androidTest/java/ml/addy/sunshine/GeneralTests.java
@@ -0,0 +1,14 @@
+package ml.addy.sunshine;
+
+import android.util.Log;
+
+import junit.framework.TestCase;
+
+public class GeneralTests extends TestCase {
+
+    public void tests() {
+        Log.v("TEST", "");
+    }
+
+
+}
