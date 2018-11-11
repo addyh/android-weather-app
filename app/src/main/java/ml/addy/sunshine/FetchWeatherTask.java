b3efbbbe4c7f92ad6f3c27a40e7473b6d4d5ba7e
addyh <13260884+addyh@users.noreply.github.com>
2018-11-10 23:47:12 -0500

    logging

--- app/src/main/java/ml/addy/sunshine/FetchWeatherTask.java
+++ app/src/main/java/ml/addy/sunshine/FetchWeatherTask.java
@@ -237,7 +237,7 @@ public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
                 inserted = mContext.getContentResolver().bulkInsert(WeatherEntry.CONTENT_URI, cvArray);
             }
 
-            Log.d(LOG_TAG, "FetchWeatherTask Complete. " + inserted + " Inserted");
+            Log.v(LOG_TAG, "FetchWeatherTask Complete. " + inserted + " Inserted");
 
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage(), e);
