b9639e628740b51eb9e0ab0fecceb1e8b2a310cd
addyh <addisonhatcher@gmail.com>
2018-11-13 18:28:25 -0500

    log forecast strings still

--- app/src/main/java/ml/addy/sunshine/ForecastAdapter.java
+++ app/src/main/java/ml/addy/sunshine/ForecastAdapter.java
@@ -24,6 +24,32 @@ public class ForecastAdapter extends CursorAdapter {
         Log.v(LOG_TAG, "ForecastAdapter()");
     }
 
+    /**
+     * Prepare the weather high/lows for presentation.
+     */
+    private String formatHighLows(double high, double low) {
+        //Log.v(LOG_TAG, "formatHighLows");
+        boolean isMetric = Utility.isMetric(mContext);
+        String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
+        return highLowStr;
+    }
+
+    /*
+        This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
+        string.
+     */
+    private String convertCursorRowToUXFormat(Cursor cursor) {
+        //Log.v(LOG_TAG, "convertCursorRowToUXFormat");
+
+        String highAndLow = formatHighLows(
+                cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP),
+                cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP));
+
+        return Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE)) +
+                " - " + cursor.getString(ForecastFragment.COL_WEATHER_DESC) +
+                " - " + highAndLow;
+    }
+
     /*
         Remember that these views are reused as needed.
      */
@@ -43,6 +69,10 @@ public class ForecastAdapter extends CursorAdapter {
         //Log.v(LOG_TAG, "bindView");
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
+
+        // Log the forecast strings
+        String forecast = convertCursorRowToUXFormat(cursor);
+        Log.v(LOG_TAG, forecast);
 
         // Read weather icon ID from cursor
         int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
